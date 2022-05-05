47bb43eb5976a8f0522107ca5fc890991fd6213e
Anton Shkolnik <anton.shkolnik@gmx.de>
2022-05-05 14:45:38 +0200

    Add testing files for scripts

---
+++ scripts/aufruf.sh
@@ -0,0 +1,11 @@
+#
+# Aufruf zur Erkennung des Beispiels
+#
+
+psaltiki4gamera.py -rotate -debug -d HA-1825.xml -o HA-sample.txt -t HA-sample-positions.txt HA-1825-sample.png
+
+#
+# erzeugt die Dateien HA-sample.txt und HA-sample-groups.txt
+# sowie Bilder debug*.png, die verschiedene Verarbeitungsschritte
+# voisualisieren 
+#
